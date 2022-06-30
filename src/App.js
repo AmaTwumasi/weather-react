9add448dcd24e54801c4bfc77b406323dbade03d
Twumasi Duoduaa Ama <91034181+AmaTwumasi@users.noreply.github.com>
2022-06-29 18:54:30 -0700

    Added axios integration to display weather of a city

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
 
-function App() {
+function App(props) {
+   function handleResponse(response) {
+     alert(`the weather is ${response.data.main.temp} in ${response.data.name}`);
+   }
+   let apiKey = "e5bb208507c0c4c97d696df0a3444983";
+   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
+
+   axios.get(apiUrl).then(handleResponse);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <code>hello world</code>
         </p>
         <a
           className="App-link"
